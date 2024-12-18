cdca80aea97c2543b693cf6d463f685546608ac7
einGlasRotwein <julivnagel@gmail.com>
2024-12-18 12:51:27 +0100

    day 13 part 3 was sponsored by floating point precision

--- code/day13.R
+++ code/day13.R
@@ -1,5 +1,5 @@
-day13 <- readLines("./inputs/13_input_exp.txt")
-# day13 <- readLines("./inputs/13_input.txt")
+# day13 <- readLines("./inputs/13_input_exp.txt")
+day13 <- readLines("./inputs/13_input.txt")
 
 day13 <- split(day13, cumsum(day13 == ""))
 day13 <- lapply(day13, function(x) x[x != ""])
@@ -15,6 +15,9 @@ b_y <- as.numeric(gsub(".*Y\\+(\\d+).*", "\\1", btn_b))
 prize_x <- as.numeric(gsub(".*X\\=(\\d+),.*", "\\1", prize))
 prize_y <- as.numeric(gsub(".*Y\\=(\\d+).*", "\\1", prize))
 
+options(scipen = 999)
+tolerance <- 0.001 # floating point precision :-)))
+
 ## PART 1 --------------------------------------------------------------------------------
 
 start <- Sys.time()
@@ -25,36 +28,23 @@ max_presses <- 100
 
 for (i in seq_along(a_x)) {
 
-  # A
-  a_presses <- 1:max_presses
-  dist_a_x <- a_x[i] * a_presses
-  dist_a_x <- dist_a_x[dist_a_x <= prize_x[i]]
+  # rearrange first equation (a button) and put into second equation (b button)
+  a_presses <- 
+  (prize_y[i] - (b_y[i] * prize_x[i] / b_x[i])) / (a_y[i] + (b_y[i] * (a_x[i] / b_x[i]) * -1))
 
-  dist_a_y <- a_y[i] * a_presses
-  dist_a_x <- dist_a_x[dist_a_y <= prize_y[i]]
-  dist_a_y <- dist_a_y[dist_a_y <= prize_y[i]]
+  # calculate b presses
+  b_presses <- (prize_x[i] - a_x[i] * a_presses) / b_x[i]
 
-  # B
-  remaining_x <- prize_x[i] - dist_a_x
-  remaining_y <- prize_y[i] - dist_a_y
+  # deal with floating point precision
+  a_presses <- ifelse(abs(a_presses - round(a_presses)) < tolerance, round(a_presses), a_presses)
+  b_presses <- ifelse(abs(b_presses - round(b_presses)) < tolerance, round(b_presses), b_presses)
 
-  possible_b_x <- remaining_x[floor(remaining_x / b_x[i]) == remaining_x / b_x[i]]
-  possible_b_y <- remaining_y[floor(remaining_y / b_y[i]) == remaining_y / b_y[i]]
-
-  n_b <- (possible_b_y / b_y[i])[(possible_b_y / b_y[i]) %in% (possible_b_x / b_x[i])]
-
-  move_bx <- b_x[i] * n_b
-  move_by <- b_y[i] * n_b
-
-  # caculate back corresponding x values
-  n_a_x <- (prize_x[i] - move_bx) / a_x[i]
-  n_a_y <- (prize_y[i] - move_by) / a_y[i]
-
-  n_a <- n_a_y[n_a_y == n_a_x]
-  n_b <- n_b[n_a_y == n_a_x]
-
-  # calculate sum(s)
-  if (length(n_a) != 0 | length(n_b) != 0) n_tokens[i] <- min(n_a * 3 + n_b)
+  if (ceiling(a_presses) == a_presses & ceiling(b_presses) == b_presses) {
+    n_tokens[i] <- 
+    ifelse(a_presses <= max_presses & b_presses <= max_presses, 3 * a_presses + b_presses, NA)
+  } else {
+    n_tokens[i] <- NA
+  }
 
 }
 
@@ -64,4 +54,34 @@ Sys.time() - start
 
 ## PART 2 --------------------------------------------------------------------------------
 
-# part 2 needs math, which I don't know :-)
+start <- Sys.time()
+
+n_tokens <- vector("numeric", length(a_x))
+
+prize_x <- prize_x + 10000000000000
+prize_y <- prize_y + 10000000000000
+
+for (i in seq_along(a_x)) {
+  
+  # rearrange first equation (a button) and put into second equation (b button)
+  a_presses <- 
+  (prize_y[i] - (b_y[i] * prize_x[i] / b_x[i])) / (a_y[i] + (b_y[i] * (a_x[i] / b_x[i]) * -1))
+  
+  # calculate b presses
+  b_presses <- (prize_x[i] - a_x[i] * a_presses) / b_x[i]
+  
+  # deal with floating point precision
+  a_presses <- ifelse(abs(a_presses - round(a_presses)) < tolerance, round(a_presses), a_presses)
+  b_presses <- ifelse(abs(b_presses - round(b_presses)) < tolerance, round(b_presses), b_presses)
+  
+  if (identical(ceiling(a_presses), a_presses) & identical(ceiling(b_presses), b_presses)) {
+    n_tokens[i] <- 3 * a_presses + b_presses
+  } else {
+    n_tokens[i] <- NA
+  }
+  
+}
+
+sum(n_tokens, na.rm = TRUE)
+
+Sys.time() - start
